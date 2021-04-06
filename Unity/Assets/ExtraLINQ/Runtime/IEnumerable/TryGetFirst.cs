53fd1b89ac23a70a20ee12f9a76a693edabc17fd
kyubuns <kyubuns@gmail.com>
2021-04-06 14:43:57 +0900

    feat: predicate無しバージョンのTryGetFirst

--- Unity/Assets/ExtraLINQ/Runtime/IEnumerable/TryGetFirst.cs
+++ Unity/Assets/ExtraLINQ/Runtime/IEnumerable/TryGetFirst.cs
@@ -5,6 +5,20 @@ namespace ExtraLinq
 {
     public static partial class EnumerableExtensions
     {
+        public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, out TSource result)
+        {
+            ThrowIf.Argument.IsNull(source, "source");
+
+            result = default;
+            foreach (var item in source)
+            {
+                result = item;
+                return true;
+            }
+
+            return false;
+        }
+
         public static bool TryGetFirst<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, out TSource result)
         {
             ThrowIf.Argument.IsNull(source, "source");
