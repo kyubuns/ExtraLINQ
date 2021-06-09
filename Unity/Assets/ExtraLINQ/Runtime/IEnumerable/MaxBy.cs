f8fcafddd4a5be9b76c2f972d1f08c8463b86600
kyubuns <kyubuns@gmail.com>
2021-06-09 19:09:38 +0900

    feat: MinByOrDefault, MaxByOrDefault

--- Unity/Assets/ExtraLINQ/Runtime/IEnumerable/MaxBy.cs
+++ Unity/Assets/ExtraLINQ/Runtime/IEnumerable/MaxBy.cs
@@ -29,5 +29,30 @@ namespace ExtraLinq
                 return value;
             }
         }
+
+        public static T MaxByOrDefault<T, TComparable>(this IEnumerable<T> source, Func<T, TComparable> comparer) where TComparable : IComparable<TComparable>
+        {
+            ThrowIf.Argument.IsNull(source, "source");
+            ThrowIf.Argument.IsNull(comparer, "comparer");
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                if (!enumerator.MoveNext()) return default;
+                var value = enumerator.Current;
+                var index = comparer(value);
+                while (enumerator.MoveNext())
+                {
+                    var v = enumerator.Current;
+                    var t = comparer(v);
+
+                    if (index.CompareTo(t) > 0) continue;
+
+                    value = v;
+                    index = t;
+                }
+
+                return value;
+            }
+        }
     }
 }
